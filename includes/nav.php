5a1658aa21f923cfc62623619b172c96ff510a99
Karl Nuttall <karl@reachstudios.co.uk>
2017-04-11 13:41:55 +0100

    Update 11 Apr
    Dashboard and fees/status page

---
+++ includes/nav.php
@@ -0,0 +1,48 @@
+<section>
+  <aside id="leftsidebar" class="sidebar">
+    <div class="user-info">
+      <div class="info-container">
+        <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Karl Nuttall</div>
+        <div class="email">karlnuttall@outlook.com</div>
+        <div class="btn-group user-helper-dropdown">
+          <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
+          <ul class="dropdown-menu pull-right">
+            <li><a href="javascript:void(0);"><i class="material-icons">assessment</i>My Fees</a></li>
+            <li><a href="javascript:void(0);"><i class="material-icons">person</i>My Profile</a></li>
+            <li><a href="javascript:void(0);"><i class="material-icons">input</i>Sign Out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="menu">
+      <ul class="list">
+        <li class="header">NAVIGATION</li>
+        <li class="active">
+          <a href="index.php">
+            <i class="material-icons">apps</i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="javascript:void(0);" class="menu-toggle">
+            <i class="material-icons">settings</i>
+            <span>Administration</span>
+          </a>
+          <ul class="ml-menu">
+            <li><a href="#">Users</a></li>
+            <li><a href="#">Usertypes</a></li>
+            <li><a href="#">Teams</a></li>
+            <li><a href="#">Fixtures &amp; Results</a></li>
+            <li><a href="#">Competitions</a></li>
+            <li><a href="fees-status.php">Fees &amp; Status</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="legal">
+      <div class="copyright">
+        &copy; 2017 <a href="javascript:void(0);"><span>team</span>suite</a>.
+      </div>
+    </div>
+  </aside>
+</section>
